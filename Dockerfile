e914adb8a107711804f956f3186cfd32c56a2747
florimondmanca <florimond.manca@gmail.com>
2018-03-10 20:46:46 +0000

    execute setup in start.sh script

--- Dockerfile
+++ Dockerfile
@@ -9,14 +9,4 @@ RUN pip3 install gunicorn
 
 WORKDIR /oser-backend/oser_backend
 
-# Collect static files
-RUN python3 manage.py collectstatic
-
-# Initialize database
-RUN python3 manage.py makemigrations
-RUN python3 manage.py migrate
-
-# Initialize admin users
-RUN python3 manage.py initadmin
-
 CMD sh ../start.sh
--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Collect static files
+python3 manage.py collectstatic
+
+# Initialize database
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# Initialize admin users
+python3 manage.py initadmin
+
+# Run server
 exec gunicorn oser_backend.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers 3
