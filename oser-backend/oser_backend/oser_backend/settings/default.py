eae231345234e379885f253f5972443d12b1fb86
florimondmanca <florimond.manca@gmail.com>
2018-03-11 11:36:55 +0000

    new version of dockerized

--- oser-backend/oser_backend/oser_backend/settings/default.py
+++ oser-backend/oser_backend/oser_backend/settings/default.py
@@ -201,4 +201,4 @@ STATIC_ROOT = os.path.join(dn(BASE_DIR), 'static')
 # User-uploaded media files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(dn(BASE_DIR), 'media')
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Run server
-exec gunicorn oser_backend.wsgi:application \
-  --bind 0.0.0.0:8000 \
-  --workers 3
