ff66305b39b6e8e41209f9a0a6a34c9a6fd3dc1b
florimondmanca <florimond.manca@gmail.com>
2018-03-18 12:33:09 +0000

    link bucket name to env var

--- oser_backend/oser_backend/settings/production.py
+++ oser_backend/oser_backend/settings/production.py
@@ -20,7 +20,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Credentials
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'oser-backend-files'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
