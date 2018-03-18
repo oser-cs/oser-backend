f43ebfcda4a3825f38968a4de2d32ad363b6b82c
florimondmanca <florimond.manca@gmail.com>
2018-03-18 17:14:51 +0000

    update ALLOWED_HOSTS

--- oser_backend/oser_backend/settings/production.py
+++ oser_backend/oser_backend/settings/production.py
@@ -7,8 +7,12 @@ from .dev import *
 from aws.conf import *
 
 DEBUG = os.environ.get('DEBUG', False)
-ALLOWED_HOSTS = ['florimondmanca.pythonanywhere.com', 'localhost',
-                 'oser-backend-production.herokuapp.com']
+ALLOWED_HOSTS = [
+    'florimondmanca.pythonanywhere.com',
+    'localhost',
+    'oser-backend-production.herokuapp.com',
+    'oser-cs.fr',
+]
 
 
 # Activate automatic Heroku settings configuration
