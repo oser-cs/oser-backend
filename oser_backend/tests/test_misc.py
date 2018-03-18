4d854bb5711be47c16f4336e4bb5cb21074dc2cd
florimondmanca <florimond.manca@gmail.com>
2018-03-18 13:49:06 +0000

    fix import bug in test_misc

--- oser_backend/tests/test_misc.py
+++ oser_backend/tests/test_misc.py
@@ -7,10 +7,10 @@ class TestLoadSettings(TestCase):
     """Test that settings can be loaded correctly."""
 
     def test_load_production_settings(self):
-        from oser_backend.settings.production import *
+        from oser_backend.settings import production
 
     def test_load_dev_settings(self):
-        from oser_backend.settings.dev import *
+        from oser_backend.settings import dev
 
     def test_load_aws_conf(self):
-        from aws.conf import *
+        from aws import conf
