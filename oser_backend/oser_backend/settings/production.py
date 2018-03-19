fe4ef01182a7e6207d2e8c6ee060952694498493
florimondmanca <florimond.manca@gmail.com>
2018-03-19 00:54:01 +0000

    update ALLOWED_HOSTS

--- oser_backend/oser_backend/settings/production.py
+++ oser_backend/oser_backend/settings/production.py
@@ -9,6 +9,6 @@ DEBUG = os.environ.get('DEBUG', False)
 ALLOWED_HOSTS = [
     'florimondmanca.pythonanywhere.com',
     'localhost',
-    'oser-backend-production.herokuapp.com',
+    'oser-backend.herokuapp.com',
     'oser-cs.fr',
 ]
