647c5f688e17673eb2478b9b650d49dbaefa28ee
florimondmanca <florimond.manca@gmail.com>
2018-04-13 23:36:56 +0100

    improve emergency contact admin

--- oser_backend/register/admin.py
+++ oser_backend/register/admin.py
@@ -1,6 +1,7 @@
 """Register admin panels."""
 
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Registration, EmergencyContact
 
 # Register your models here.
@@ -10,7 +11,7 @@ from .models import Registration, EmergencyContact
 class RegistrationAdmin(admin.ModelAdmin):
     """Admin panel for registrations."""
 
-    list_display = ('id', 'first_name', 'last_name', 'submitted')
+    list_display = ('last_name', 'first_name', 'submitted')
     readonly_fields = ('submitted',)
     list_filter = ('submitted',)
     autocomplete_fields = ('address', 'emergency_contact')
@@ -20,7 +21,16 @@ class RegistrationAdmin(admin.ModelAdmin):
 class EmergencyContactAdmin(admin.ModelAdmin):
     """Admin panel for emergency contacts."""
 
-    list_display = ('last_name', 'first_name', 'contact', 'registration',)
+    list_display = ('last_name', 'first_name', 'contact', 'registration_link',)
+    list_display_links = ('registration_link',)
 
     # necessary to use emergency contact in Registration's admin autocomplete
     search_fields = ('last_name', 'first_name',)
+
+    def registration_link(self, obj):
+        """Link to the contact's registration object."""
+        url = '/admin/register/registration/{}'.format(obj.registration.pk)
+        return format_html(
+            '<a href="{}">{}</a>',
+            url, str(obj.registration)
+        )
