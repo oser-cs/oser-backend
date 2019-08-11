64790b0a2386a57286daf8664508294d53b6488e
chiahetcho <leila.bekaddour@student-cs.fr>
2019-08-11 14:33:20 +0200

    Try to make the send reset password email work

---
+++ oser_backend/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import PasswordResetForm
+from django.conf import settings
+from django.utils.translation import gettext as _
+from rest_framework import serializers
+
+###### IMPORT YOUR USER MODEL ######
+from users.models import User
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password_reset_form_class = PasswordResetForm
+    def validate_email(self, value):
+        self.reset_form = self.password_reset_form_class(data=self.initial_data)
+        if not self.reset_form.is_valid():
+            raise serializers.ValidationError(_('Error'))
+
+        ###### FILTER YOUR USER MODEL ######
+        if not User.objects.filter(email=value).exists():
+
+            raise serializers.ValidationError(_('Invalid e-mail address'))
+        return value
+
+    def save(self):
+        request = self.context.get('request')
+        opts = {
+            'use_https': request.is_secure(),
+            'from_email': getattr(settings, 'DEFAULT_FROM_EMAIL'),
+
+            ###### USE YOUR TEXT FILE ######
+            'email_template_name': 'email-reset-template.txt',
+
+            'request': request,
+        }
+        self.reset_form.save(**opts)
--- oser_backend/settings/common.py
+++ oser_backend/settings/common.py
@@ -230,11 +230,32 @@ LOGGING = {
     },
 }
 
+# Connect custom PasswordResetSerializer to override default
 
+REST_AUTH_SERIALIZERS = {
+    'PASSWORD_RESET_SERIALIZER': 
+        'oser_backend.serializers.PasswordResetSerializer',
+}
+
+DEFAULT_FROM_EMAIL = "admin@oser-cs.fr"
 # Email configuration
 
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
-SENDGRID_API_KEY = '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'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
+# Sendgrid configuration 
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Toggle sandbox mode (when running in DEBUG mode)
+SENDGRID_SANDBOX_MODE_IN_DEBUG=False
+
+# echo to stdout or any other file-like object that is passed to the backend via the stream kwarg.
+SENDGRID_ECHO_TO_STDOUT=True
 
 # Mails app config
 
